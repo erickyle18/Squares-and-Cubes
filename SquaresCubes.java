fa1412ca324021866bd60a854ee25a2d76294459
Eric Isaac <40010155+erickyle18@users.noreply.github.com>
2021-02-16 16:11:48 -0500

    Create SquaresCubes.java

---
+++ SquaresCubes.java
@@ -0,0 +1,36 @@
+//Print the squares and cubes of the numbers 0-10 in table format
+
+public class SquaresCubes {
+	
+	public static void main(String[] args) {
+		
+		//Print table header
+		System.out.printf("%s\t%s\t%s\n", "number", "square", "cube");
+		
+		//Print out the table with corresponding numbers
+		int x = 0;
+		System.out.printf("%d\t%d\t%d\n", (x),(x * x),(x * x * x));
+		x = 1;
+		System.out.printf("%d\t%d\t%d\n", (x),(x * x),(x * x * x));
+		x = 2;
+		System.out.printf("%d\t%d\t%d\n", (x),(x * x),(x * x * x));
+		x = 3;
+		System.out.printf("%d\t%d\t%d\n", (x),(x * x),(x * x * x));
+		x = 4;
+		System.out.printf("%d\t%d\t%d\n", (x),(x * x),(x * x * x));
+		x = 5;
+		System.out.printf("%d\t%d\t%d\n", (x),(x * x),(x * x * x));
+		x = 6;
+		System.out.printf("%d\t%d\t%d\n", (x),(x * x),(x * x * x));
+		x = 7;
+		System.out.printf("%d\t%d\t%d\n", (x),(x * x),(x * x * x));
+		x = 8;
+		System.out.printf("%d\t%d\t%d\n", (x),(x * x),(x * x * x));
+		x = 9;
+		System.out.printf("%d\t%d\t%d\n", (x),(x * x),(x * x * x));
+		x = 10;
+		System.out.printf("%d\t%d\t%d\n", (x),(x * x),(x * x * x));
+		
+	}
+
+}
